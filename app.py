1f839bed62eb0bb5d476c90f4ccf620c670a8721
Nur Imam Hawari Akbar <imamhawari19@gmail.com>
2019-05-13 06:45:27 +0700

    create endpoint and template

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('index.html', title='Home')
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+@app.route("/profile")
+def profile():
+    return render_template('profile.html', title='Profile')
+
+@app.route("/login")
+def login():
+    return render_template('login.html', title='Login')
+
+@app.route("/register")
+def register():
+    return render_template('register.html', title='Register')    
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
